993f9ce85583c32d72cf37dd4222280dc62cab00
Till Hainbach <till.hainbach@posteo.de>
2021-03-03 18:38:44 +0100

    restart

---
+++ SwiftUISparkleTestApp/SwiftUISparkleTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISparkleTestAppApp.swift
+//  SwiftUISparkleTestApp
+//
+//  Created by Till Hainbach on 03.03.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISparkleTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
