943036f9ffa85385e2b460745f0e79aeef898cb3
Till Hainbach <Till.Hainbach@posteo.de>
2021-04-18 15:51:11 +0200

    refactor: expose underlying SPUUpdater via public methods

--- SwiftUISparkleTestApp/SparkleAutoUpdater.swift
+++ SwiftUISparkleTestApp/SparkleAutoUpdater.swift
@@ -10,23 +10,28 @@ import os
 import Combine
 
 
-class SparkleAutoUpdater: NSObject, ObservableObject {
-
-  var updater: SPUUpdater?
-  let standardUserDriver: SPUStandardUserDriver = SPUStandardUserDriver(hostBundle: Bundle.main, delegate: nil)
-
+public final class SparkleAutoUpdater: NSObject, ObservableObject {
   @Published var canCheckForUpdates: Bool = false
 
-  var cancellable: AnyCancellable?
+  /// store SPUUPdater and SPUStandardUserDriver
+  private var updater: SPUUpdater?
+  private let standardUserDriver: SPUStandardUserDriver = SPUStandardUserDriver(hostBundle: Bundle.main, delegate: nil)
 
-  override init() {
+  private var cancellable: AnyCancellable?
+
+  public init(userDriver: SPUUserDriver? = nil, delegate: SPUUpdaterDelegate? = nil) {
     super.init()
-    setUpdater()
+    setUpdater(userDriver: userDriver, delegate: delegate)
   }
 
-  func setUpdater() {
-    updater = SPUUpdater(hostBundle: Bundle.main, applicationBundle: Bundle.main, userDriver: self, delegate: nil)
-    updater?.httpHeaders = ["Accept": "application/octet-stream"]
+  /// Check for updates
+  public func checkForUpdates() {
+    updater?.checkForUpdates()
+  }
+
+  private func setUpdater(userDriver: SPUUserDriver?, delegate: SPUUpdaterDelegate?) {
+    let userDriver = userDriver ?? standardUserDriver
+    updater = SPUUpdater(hostBundle: Bundle.main, applicationBundle: Bundle.main, userDriver: userDriver, delegate: delegate)
     // Subscribe to didFinishLaunchingNotification and start updater
     // when application finished launching
     cancellable = NotificationCenter.default.publisher(for: NSApplication.didFinishLaunchingNotification)
@@ -34,146 +39,135 @@ class SparkleAutoUpdater: NSObject, ObservableObject {
       .sink { [weak self] _ in self?.start() }
   }
 
-  func start() {
+  private func start() {
     do {
       try updater?.start()
     } catch {
-      Logger().error("Failed to start SPUUpdater with error: \(error.localizedDescription)")
+      // Post Failure notification. Also an alert to be presented to the user or to be logged
+      NotificationCenter.default.post(name: Self.didFailStartingUpdater, object: error)
       return
     }
   }
 
-  func checkForUpdates() {
-    updater?.checkForUpdates()
-  }
-
-
 }
 
+//MARK: - Extension: Notification.Name
 extension SparkleAutoUpdater {
-  func authorize(with keychainService: String) {
-    guard let token = retrieveToken(from: keychainService) else {
-      return
-    }
+  static let didFailStartingUpdater = Notification.Name("SPUUpdater-start-failed")
+}
 
-    updater?.httpHeaders?["Authorization"] = "Token \(token)"
-  }
-
-  func retrieveToken(from keychainService: String) -> String? {
-    let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
-                                kSecAttrService as String: keychainService,
-                                kSecReturnData as String: kCFBooleanTrue!,
-                                kSecMatchLimit as String: kSecMatchLimitOne]
-
-    var item: CFTypeRef?
-    let status = SecItemCopyMatching(query as CFDictionary, &item)
-    guard status != errSecItemNotFound else {
-      Logger().error("SparkleAutoUpdater was unable to find Github Token.")
-      return nil
-    }
-    guard status == errSecSuccess else {
-      let errorMessage = SecCopyErrorMessageString(status, nil) as String? ?? "Did not understand KeychainError code"
-      Logger().error("\(errorMessage)")
-      return nil
-    }
-    guard let tokenData = item as? Data,
-          let token = String(data: tokenData, encoding: .utf8) else {
-      Logger().error("unexpected password data")
-      return nil
-    }
-
-    return token
+//MARK: - Extension HTTPHeaders
+extension SparkleAutoUpdater {
+  public func setHTTPHeaders(_ value: String, for key: String) {
+    updater?.httpHeaders?[key] = value
   }
 }
 
+//MARK: - Extention: Authorization
+extension SparkleAutoUpdater {
+  public func authorize(with token: String) {
+    setHTTPHeaders("Token \(token)", for: "Authorization")
+  }
+
+}
+
+//MARK: - Extension: GitHub Convenience
+extension SparkleAutoUpdater {
+  public func connectToGithub(with token: String) {
+    authorize(with: token)
+    setHTTPHeaders("application/octet-stream", for: "Accept")
+  }
+}
+
+//MARK: - Extension: Implemention of SPUUserDriver
 extension SparkleAutoUpdater: SPUUserDriver {
 
-  func showCanCheck(forUpdates canCheckForUpdates: Bool) {
+  public func showCanCheck(forUpdates canCheckForUpdates: Bool) {
     standardUserDriver.showCanCheck(forUpdates: canCheckForUpdates)
     self.canCheckForUpdates = canCheckForUpdates
   }
 
-  func show(_ request: SPUUpdatePermissionRequest, reply: @escaping (SUUpdatePermissionResponse) -> Void) {
+  public func show(_ request: SPUUpdatePermissionRequest, reply: @escaping (SUUpdatePermissionResponse) -> Void) {
     standardUserDriver.show(request, reply: reply)
   }
 
-  func showUserInitiatedUpdateCheck(completion updateCheckStatusCompletion: @escaping (SPUUserInitiatedCheckStatus) -> Void) {
+  public func showUserInitiatedUpdateCheck(completion updateCheckStatusCompletion: @escaping (SPUUserInitiatedCheckStatus) -> Void) {
     standardUserDriver.showUserInitiatedUpdateCheck(completion: updateCheckStatusCompletion)
   }
 
-  func dismissUserInitiatedUpdateCheck() {
+  public func dismissUserInitiatedUpdateCheck() {
     standardUserDriver.dismissUserInitiatedUpdateCheck()
   }
 
-  func showUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUUpdateAlertChoice) -> Void) {
+  public func showUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUUpdateAlertChoice) -> Void) {
     standardUserDriver.showUpdateFound(with: appcastItem, userInitiated: userInitiated, reply: reply)
   }
 
-  func showDownloadedUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUUpdateAlertChoice) -> Void) {
+  public func showDownloadedUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUUpdateAlertChoice) -> Void) {
     standardUserDriver.showDownloadedUpdateFound(with: appcastItem, userInitiated: userInitiated, reply: reply)
   }
 
-  func showResumableUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUInstallUpdateStatus) -> Void) {
+  public func showResumableUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUInstallUpdateStatus) -> Void) {
     standardUserDriver.showResumableUpdateFound(with: appcastItem, userInitiated: userInitiated, reply: reply)
   }
 
-  func showInformationalUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUInformationalUpdateAlertChoice) -> Void) {
+  public func showInformationalUpdateFound(with appcastItem: SUAppcastItem, userInitiated: Bool, reply: @escaping (SPUInformationalUpdateAlertChoice) -> Void) {
     standardUserDriver.showInformationalUpdateFound(with: appcastItem, userInitiated: userInitiated, reply: reply)
   }
 
-  func showUpdateReleaseNotes(with downloadData: SPUDownloadData) {
+  public func showUpdateReleaseNotes(with downloadData: SPUDownloadData) {
     standardUserDriver.showUpdateReleaseNotes(with: downloadData)
   }
 
-  func showUpdateReleaseNotesFailedToDownloadWithError(_ error: Error) {
+  public func showUpdateReleaseNotesFailedToDownloadWithError(_ error: Error) {
     standardUserDriver.showUpdateReleaseNotesFailedToDownloadWithError(error)
   }
 
-  func showUpdateNotFoundWithError(_ error: Error, acknowledgement: @escaping () -> Void) {
+  public func showUpdateNotFoundWithError(_ error: Error, acknowledgement: @escaping () -> Void) {
     standardUserDriver.showUpdateNotFoundWithError(error, acknowledgement: acknowledgement)
   }
 
-  func showUpdaterError(_ error: Error, acknowledgement: @escaping () -> Void) {
+  public func showUpdaterError(_ error: Error, acknowledgement: @escaping () -> Void) {
     standardUserDriver.showUpdaterError(error, acknowledgement: acknowledgement)
   }
 
-  func showDownloadInitiated(completion downloadUpdateStatusCompletion: @escaping (SPUDownloadUpdateStatus) -> Void) {
+  public func showDownloadInitiated(completion downloadUpdateStatusCompletion: @escaping (SPUDownloadUpdateStatus) -> Void) {
     standardUserDriver.showDownloadInitiated(completion: downloadUpdateStatusCompletion)
   }
 
-  func showDownloadDidReceiveExpectedContentLength(_ expectedContentLength: UInt64) {
+  public func showDownloadDidReceiveExpectedContentLength(_ expectedContentLength: UInt64) {
     standardUserDriver.showDownloadDidReceiveExpectedContentLength(expectedContentLength)
   }
 
-  func showDownloadDidReceiveData(ofLength length: UInt64) {
+  public func showDownloadDidReceiveData(ofLength length: UInt64) {
     standardUserDriver.showDownloadDidReceiveData(ofLength: length)
   }
 
-  func showDownloadDidStartExtractingUpdate() {
+  public func showDownloadDidStartExtractingUpdate() {
     standardUserDriver.showDownloadDidStartExtractingUpdate()
   }
 
-  func showExtractionReceivedProgress(_ progress: Double) {
+  public func showExtractionReceivedProgress(_ progress: Double) {
     standardUserDriver.showExtractionReceivedProgress(progress)
   }
 
-  func showReady(toInstallAndRelaunch installUpdateHandler: @escaping (SPUInstallUpdateStatus) -> Void) {
+  public func showReady(toInstallAndRelaunch installUpdateHandler: @escaping (SPUInstallUpdateStatus) -> Void) {
     standardUserDriver.showReady(toInstallAndRelaunch: installUpdateHandler)
   }
 
-  func showInstallingUpdate() {
+  public func showInstallingUpdate() {
     standardUserDriver.showInstallingUpdate()
   }
 
-  func showSendingTerminationSignal() {
+  public func showSendingTerminationSignal() {
     standardUserDriver.showSendingTerminationSignal()
   }
 
-  func showUpdateInstallationDidFinish(acknowledgement: @escaping () -> Void) {
+  public func showUpdateInstallationDidFinish(acknowledgement: @escaping () -> Void) {
     standardUserDriver.showUpdateInstallationDidFinish(acknowledgement: acknowledgement)
   }
 
-  func dismissUpdateInstallation() {
+  public func dismissUpdateInstallation() {
     standardUserDriver.dismissUpdateInstallation()
   }
 
